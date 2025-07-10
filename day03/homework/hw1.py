4c42a1e2d07bd1f1a53b9f7fbb67695ec63a4644
giorgikandelaki123 <gio.kand2013@gmail.com>
2025-07-10 21:34:27 +0400

    1

---
+++ day03/homework/hw1.py
@@ -0,0 +1,43 @@
+car_speed=198
+human_speed=36
+sum_of_speed=car_speed+human_speed
+
+minus_of_speed=car_speed-human_speed
+
+multiply_speed=car_speed*human_speed
+
+devide_of_speed=car_speed/human_speed
+
+
+name="giorgi"
+name="ilia"
+name="gabrieli"
+name="saba"
+name="mariami"
+print(name)
+
+name="giorgi"
+Name="saba"
+nAme="brian"
+naMe="don"
+namE="ilia"
+print(name,Name,nAme,naMe,namE)
+
+name = "giorgi"         #ცვლადის სახელში არ უნდა იყოს რიცხვი
+user_name="bubunauri"   #ცვლადის სახელში არ უნდა იყოს {
+user_name = "goga"      #ცვლადის მნიშვნელობას როცა სტრინგია უნდა ჰქონდეს ""
+user_urname = "axalia"  #ცვლადის სახელში არ უნდა იყოს გამოყენებული - და სტრინგის მნიშვნელობას აკლია ""
+
+
+
+my_food="xinkali"
+my_fruit="banani"
+my_snack="rafaelo"
+my_car="mercedes"
+my_sport="basketball"
+print(my_food+my_fruit+my_snack+my_car+my_sport)
+
+
+car="audi"
+number=10
+print(car*number)
