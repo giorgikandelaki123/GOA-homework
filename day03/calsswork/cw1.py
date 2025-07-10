a172b0f0758d7889695d825b87f88c2facb24085
giorgikandelaki123 <gio.kand2013@gmail.com>
2025-07-10 19:04:58 +0400

    1

---
+++ day03/calsswork/cw1.py
@@ -0,0 +1,36 @@
+animal = "cow"
+print(animal)
+animal = "pig"
+print(animal) 
+
+food = "lobio"
+Food = "xawapuri"
+fOod = "pizza"
+fooD = "qatami"
+foOd = "mwvadi"
+print(food, Food, fOod,fooD,foOd)
+
+
+
+
+
+my_name="giorgi"
+my_keyboard="logitech"
+my_mouse="razer"
+race_car="ferrari"
+
+my_age=11
+my_heigth=152
+rooler_lenht=10
+my_speed=20
+
+my_weight=40.5
+rocket_speed=1384.7
+car_speed=234.5
+my_ping=5.5
+
+
+user_name = "goga"
+user_surname = "chalauri"
+first_number = 200
+second_number = 123
