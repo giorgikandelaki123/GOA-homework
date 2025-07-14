4dbf953e6ca0ed91837634597c9293072d883c8e
giorgikandelaki123 <gio.kand2013@gmail.com>
2025-07-14 19:48:06 +0400

    1

---
+++ day04/classwork/cw1.py
@@ -0,0 +1,20 @@
+#print("enter your name")
+#name=input()
+#print(name)
+
+
+#print("please enter num1")
+#num1 = int ( input() )
+
+#print("please enter num2")
+#num2 = int ( input() )
+
+#print("რიცხვების ჯამი არის:", num1 + num2)""
+
+
+
+#მომხმარებელს შემოატანინეთ თავისი ასაკი, თქვენი დავალებაა შემოყვანილი მნიშვნელობა გადაიყვანოთ integer ტიპის მონმაცემად და საბოლოოდ დაბეჭდოთ მომხმარებლის მიერ შემოყვანილი მნიშვნელობის მონაცემთა ტიპი. 
+
+age = int(("11?"))
+print()
+
