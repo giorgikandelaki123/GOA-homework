ece484c75a52095c6974e668aa34cf55c0e9eff2
giorgikandelaki123 <gio.kand2013@gmail.com>
2025-07-27 18:29:17 +0400

    1

---
+++ day08/classwork/cw1.py
@@ -0,0 +1,10 @@
+print(50>10 and 50<100)
+print(10>4 and 10==10)
+print(70>10 and 20<30)
+print(20>40 or 10>24)
+print(90==10 or 100>200)
+print(20==10 or 10>525)
+
+
+for x in range(20):
+    print("giorgi")
