59a2b454a49763b52775ce8cc814ccdcae07c7c4
giorgikandelaki123 <gio.kand2013@gmail.com>
2025-08-03 16:54:28 +0400

    1

---
+++ day10/homework/hw1.py
@@ -0,0 +1,47 @@
+#1
+for i in range(0,38):
+    print(i)
+
+#2
+for i in range(30,40):
+    print(i)
+
+#3
+for i in range(12,120):
+    print(i)
+
+#4
+name="giorgi"
+age=11
+for i in range(18):
+    print(ghfte4fffy5r55ewd)
+
+
+#5
+for i in range(10,100,3):
+    print(i)
+
+#6    
+for i in range(30):
+    print(str(i) + " ფორთოხალი")
+
+#7    
+age=input("შემოიყვანე მისი ასაკი")
+for i in range(13):
+    print(age)
+
+
+
+#8
+x=1
+while x<11:
+    print(x)
+    x=x+1
+    
+#9
+y=0
+while y<24:
+    print(y)
+    y=y+1
+
+#10    
