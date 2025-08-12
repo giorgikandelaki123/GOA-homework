6e1bf828eadf668c5354080dc3342b247d6071ad
giorgikandelaki123 <gio.kand2013@gmail.com>
2025-08-12 12:32:00 +0400

    1

---
+++ day12/homework/hw1.py
@@ -0,0 +1,36 @@
+#1
+# Conditional statement გვეხმარება პროგრამაში მივიღოთ გადაწყვეტილება.
+# ანუ, როცა გვინდა კოდი განსაზღვრულ შემთხვევებში შესრულდეს,
+# ვიყენებთ პირობით განცხადებებს.
+
+
+#2
+for i in range(50):
+    print("hello world")
+
+
+#3
+x=3
+while x<50:
+    print(x)
+    x=x+1    
+
+
+#4
+password = input("Enter your password: ")
+if password == "1234":
+    print("Password is correct")
+else:
+    print("Password is incorrect") 
+
+
+#5
+animal = input("შეიყვანე ცხოველის სახეობა: ")
+if animal == "ძაღლი":
+    print("woaf! woaf!")
+else:
+    print("შენ არ გყავს ძაღლი")       
+
+
+#6
+"ვუყურე"
