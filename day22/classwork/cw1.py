3a5c6e8bcae2a8e1aed0ce1a1c9c58279840186f
giorgikandelaki123 <gio.kand2013@gmail.com>
2025-09-16 12:38:24 +0400

    1

---
+++ day22/classwork/cw1.py
@@ -0,0 +1,20 @@
+names = ["Dominic", "Brian", "Letty", "Roman", "Tej", "Mia", "Luke", "Han", "Gisele", "Deckard", "jacob"]
+
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+print(names[5])
+print(names[6])
+print(names[7])
+print(names[8])
+print(names[9])
+print(names[10])
+
+
+vegetables = ["კარტოფილი", "სტაფილო", "კიტრი", "პომიდორი", "ბადრიჯანი", "ბროკოლი"]
+vegetables[0] = "viri"
+vegetables[3] = "tagvit"
+vegetables[5] = "bokveri"
+print(vegetables)
