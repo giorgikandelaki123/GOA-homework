f0be448687dd1c8850ac145630f4f54a4f9f85b3
giorgikandelaki123 <gio.kand2013@gmail.com>
2025-09-23 14:14:53 +0400

    1

---
+++ day24/classwork/cw1.py
@@ -0,0 +1,25 @@
+#1
+name=["kai", "ishowspeed", "rakai", "dota", "merabiko", "gio", "mafia"]
+print(name[-1]) 
+print(name[-2])  
+print(name[-3])  
+print(name[-4])
+print(name[-5])  
+print(name[-6])  
+print(name[-7]) 
+
+
+
+#2
+name1 = "ლუკა"
+name2 = "სოფო"
+name3 = "ანდრია"
+name4 = "ქეთა"
+name5 = "გეგა"
+names_list = [name1, name2, name3, name4, name5]
+print(names_list[-1]) 
+
+#3
+name = "irakli"
+print(name[3]) 
+print(name[-3])  
