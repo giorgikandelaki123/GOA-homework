4b212d9cc1cf752e417e6b3538318a30dffe8339
giorgikandelaki123 <gio.kand2013@gmail.com>
2025-10-11 13:21:16 +0400

    1

---
+++ day29/homework/hw1.py
@@ -0,0 +1,35 @@
+#1
+text="pythonprogramming"
+print(text[:6])
+
+#2
+numbers = [10, 20, 30, 40, 50, 60, 70]
+print(numbers[-5:-2])
+
+#3
+word="HelloWorld"
+print(word[:-5])
+
+
+#4
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print(letters[-7])
+print(letters[-5])
+print(letters[-3])
+
+
+#5
+google = "Information"
+print(google[-9:-4])
+
+
+
+#6
+no_word = "abcdefghijklmno"
+slice1 = no_word[0:4]
+slice2 = no_word[9:15]
+slice3 = no_word[5:10]
+
+print(slice1)
+print(slice2)
+print(slice3)
