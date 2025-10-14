2a9d8af828b589be797a1ca3a2f1fd4fa9811f20
giorgikandelaki123 <gio.kand2013@gmail.com>
2025-10-14 22:58:53 +0400

    1

---
+++ day31/classwork/cw1.py
@@ -0,0 +1,13 @@
+#1
+list = ["ina" , "givi" , "nika" , "daviti" , "ia" , "lizi"]
+list[0:2] = ["irina" , "milana" , "kira", "mate"]
+list[-2:] = ["gia" , "emzari" , "xvicha"]
+print(list)
+
+
+#2
+num=int(input("enter number"))
+if num % 2 == 0:
+    print("EVEN")
+else:
+    print("ODD")    
