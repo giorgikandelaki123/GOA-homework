847de62d95d72537f9544f49a2fa640fdcba4ac7
giorgikandelaki123 <gio.kand2013@gmail.com>
2025-10-21 19:17:39 +0400

    1

--- day32/homework/hw1.py
+++ day32/homework/hw1.py
@@ -1,90 +1,90 @@
 #1
 for i in range(1, 51):
     if i % 2 == 0:
-        print(str(i) + " ლუწია")
+        print(i, "ლუწია")
     else:
-        print(str(i) + " კენტია")
+        print(i, "კენტი")
 
 
 #2 
 for i in range(0, 21):
     if i % 3 == 0 and i % 5 == 0:
-        print(str(i) + " იყოფა 3-ზე და 5-ზე")
+        print(i, "იყოფა 3-ზე და 5-ზე")
     elif i % 3 == 0:
-        print(str(i) + " იყოფა 3-ზე")
+        print(i, "იყოფა 3-ზე")
     elif i % 5 == 0:
-        print(str(i) + " იყოფა 5-ზე")
+        print(i, "იყოფა 5-ზე")
     else:
-        print(str(i) + " არ იყოფა არცერთზე")
+        print(i, "არ იყოფა არცერთზე")
 
 
 #3 
-number = int(input("შეიყვანე რიცხვი: "))
-even_count = 0
-odd_count = 0
-for i in range(0, number + 1):
+nn = int(input("ნომერი: "))
+luwi = 0
+kenti = 0
+for i in range(0,1):
     if i % 2 == 0:
-        even_count = even_count + 1
+        luwi += 1
     else:
-        odd_count = odd_count + 1
-
-print("ლუწი რიცხვების რაოდენობა: " + str(even_count))
-print("კენტი რიცხვების რაოდენობა: " + str(odd_count))
-
+        kenti += 1
+print("ლუწი:", luwi)
+print("კენტი:", kenti)
 
 
 #4
-numbers = [10, 25, 33, 47, 80, 99]
-for i in range(0, 6):  
-    if numbers[i] > 50:
-        print(str(numbers[i]) + " მეტი 50-ზე")
+lst = [10, 25, 33, 47, 80, 99]
+for x in lst:
+    if x > 50:
+        print(x, "მეტი 50-ზე")
     else:
-        print(str(numbers[i]) + " ნაკლები 50-ზე")
+        print(x, "ნაკლები 50-ზე")
+
 
 
 #5
-sum = 0
+total = 0
 for i in range(0, 101):
     if i % 2 == 0:
         print(i)
-        sum = sum + i
-print("ლუწი რიცხვების ჯამია: " + str(sum))
+        total += i
+print("ლუწი რიცხვების ჯამია:", total)
+
 
 
 #6
-words = ["apple", "banana", "avocado", "grape", "apricot", "melon"]
-for i in range(0, 6):  
-    if words[i][0] == "a":
-        print(words[i])
+words = ["apple", "banana", "avocado", "cherry", "apricot"]
+for w in words:
+    if w.startswith("a"):
+        print(w)
 
 
 #7
 for i in range(0, 21):
     if i == 0:
-        print(str(i) + " ნულია")
+        print(i, "ნულია")
     elif i % 2 == 0:
-        print(str(i) + " ლუწია")
+        print(i, "ლუწია")
     else:
-        print(str(i) + " კენტია")
+        print(i, "კენტია")
 
 
 #8
-numbers = [5, 15, 25, 35, 45, 55]
-for i in range(0, 6):
-    if numbers[i] % 5 == 0:
-        print(numbers[i])
+list = [5, 15, 25, 35, 45, 55]
+for x in lst:
+    if x % 5 == 0:
+        print(x)
+
 
 
 
 #9 
-word = input("შეიყვანე 5 ასოიანი სიტყვა: ")
-for i in range(0, 5):
-    print(word[i])
+s = "შენი სიტყვა"
+for ch in s:
+    print(ch)
 
 
 #10
 total = 0
 for i in range(1, 11):
-    total = total + i
-
-print("ჯამი არის: " + str(total))
+    total += i
+print("ჯამი არის:", total)
