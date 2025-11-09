3c5c8a42909a16bd67282fcab739abe6b57065a6
giorgikandelaki123 <gio.kand2013@gmail.com>
2025-11-09 08:41:26 +0400

    1

---
+++ day38/homework/hw1.py
@@ -0,0 +1,68 @@
+name = input("შეიყვანეთ თქვენი სახელი: ")
+print(name.upper())
+
+
+# 2) 
+name = input("შეიყვანეთ თქვენი სახელი დიდი ასოებით: ")
+print(name.lower())
+
+
+# 3) 
+names = input("შეიყვანეთ თქვენი სახელი პატარა ასოებით: ")
+print(name.capitalize())
+
+
+# 4) 
+arvici = ["nika", "mari", "saba", "luka"]
+for n in arvici:
+    print(n.upper())
+
+
+# 5) 
+ravici = ["NIKA", "MARI", "SABA", "LUKA"]
+for n in ravici:
+    print(n.lower())
+
+
+# 6) 
+vici = ["nika", "mari", "saba", "luka"]
+for n in vici:
+    print(n.capitalize())
+
+
+# 7)
+items = ["apple", "banana", "cherry", "orange"]
+print("სიის სიგრძეა:", len(items))
+
+
+# 8) 
+name = "ალექსანდრე"
+print("ასოების რაოდენობაა:", len(name))
+
+
+# 9) 
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+even_count = 0
+for n in numbers:
+    if n % 2 == 0:
+        even_count += 1
+print("ლუწი რიცხვების რაოდენობაა:", even_count)
+
+
+# 10)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+odd_count = 0
+for n in numbers:
+    if n % 2 != 0:
+        odd_count += 1
+print("კენტი რიცხვების რაოდენობაა:", odd_count)
+
+
+# 11) 
+start = int(input("შეიყვანეთ start რიცხვი: "))
+end = int(input("შეიყვანეთ end რიცხვი: "))
+step = int(input("შეიყვანეთ step რიცხვი: "))
+
+for i in range(start, end, step):
+    if i % 2 == 0:
+        print(i)
