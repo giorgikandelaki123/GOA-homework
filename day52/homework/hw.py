23451458db3c79fe2af4af8878490f28a6dc9870
giorgikandelaki123 <gio.kand2013@gmail.com>
2026-01-06 18:37:25 +0400

    1

---
+++ day52/homework/hw.py
@@ -0,0 +1,52 @@
+# 2
+sityva = "cuRcxElA"
+geniosi = []
+
+i = 0
+while i < len(sityva):
+    if sityva[i] == sityva[i].upper():
+        geniosi.append(sityva[i].lower())
+    else:
+        geniosi.append(sityva[i].upper())
+    i += 1
+
+print(geniosi)
+
+
+# 3
+saxelebi = ["giorgi", "ANA", "luka", "NINO", "elene", "DAVID"]
+saxelebi_1 = []
+
+for x in saxelebi:
+    if x == x.lower():
+        saxelebi_1.insert(0, x.upper())
+    elif x == x.upper():
+        saxelebi_1.append(x.lower())
+
+print(saxelebi_1)
+
+# 4
+cities = ["tbilisi", "BATUMI", "kutaisi", "TELAVI", "rustavi", "GORI"]
+y = 0  
+
+while y < len(cities):
+    if cities[y] == cities[y].upper():
+        cities.pop(y) 
+    else:
+        cities[y] = cities[y].upper() 
+        y += 1
+
+print(cities)
+
+# 8
+original = "python"
+reverse = ""
+
+for z in original:
+    reverse = z + reverse
+result = reverse.upper()
+
+print(result)
+
+# 9
+"გადავხედე"
