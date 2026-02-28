7ef3f8c699d3c87feb2835abe1103fd271acd386
giorgikandelaki123 <gio.kand2013@gmail.com>
2026-02-28 22:08:33 +0400

    1

--- day69/homework/hw.py
+++ day69/homework/hw.py
@@ -2,18 +2,15 @@
 def solution(string):
     return string[::-1]
 
+
 # 4
 def repeat_str(repeat, string):
     return repeat * string
 
 # 5
-def square_sum(numbers):
+def square_sum(numbers):      
     return sum(n**2 for n in numbers)
 
 # 6
-def number_to_string(num):
-    return str(num)
-    
-    number_to_string(123) 
-    number_to_string(999)   
-    number_to_string(-100)  
+def string_to_number(s):
+    return int(s)
