e9f109ada214dadd1e86493d00d3ee0a9728a254
giorgikandelaki123 <gio.kand2013@gmail.com>
2026-07-18 22:54:07 +0400

    1

---
+++ day101/homework/hw1.py
@@ -0,0 +1,40 @@
+# 1
+
+# def solution(digits):
+#     biggest = 0
+#     i = 0
+
+#     while i < len(digits) - 4:
+#         num = int(digits[i:i+5])
+
+#         if num > biggest:
+#             biggest = num
+
+#         i += 1
+
+#     return biggest
+
+
+
+# 2
+
+# def break_chocolate(n, m):
+#     if n == 0 or m == 0:
+#         return 0
+
+#     return n * m - 1
+
+
+
+# 3
+# def is_anagram(test, original):
+    # return sorted(test.lower()) == sorted(original.lower())
+
+
+
+# 4
+# def over_the_road(address, n):
+#     return 2 * n - address + 1
+
+
+
