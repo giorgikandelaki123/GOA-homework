c6e1dcd96950b43425275b543dd1f71603752456
giorgikandelaki123 <gio.kand2013@gmail.com>
2026-09-07 09:38:49 +0400

    1

---
+++ day123/classwork/script.js
@@ -0,0 +1,24 @@
+// 1
+console.log("Giorgi Kandelaki".toUpperCase());
+console.log("Tbilisi, Georgia".toLowerCase()); 
+
+// 2
+console.log("   Giorgi Kandelaki   ".trim());
+
+// 3
+console.log(10 + 5);
+console.log(10 - 5);
+console.log(10 * 5);
+console.log(10 / 5);
+console.log(10 % 5);
+console.log(10 ** 5);
+
+// 4
+console.log("i live in Tbilisi".replace("live", "love"));
+
+// 5
+console.log("i live in Tbilisi and also i live Georgia".replaceAll("live", "love"));
+
+// 6
+console.log("Tbilisi".repeat(10));
+
