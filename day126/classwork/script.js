f555655d1325049a02abef1222cbc161fea6ef3b
giorgikandelaki123 <gio.kand2013@gmail.com>
2026-09-12 14:49:31 +0400

    1

---
+++ day126/classwork/script.js
@@ -0,0 +1,18 @@
+// 1
+let firstName = prompt("შეიყვანე სახელი:");
+let lastName = prompt("შეიყვანე გვარი:");
+let age = prompt("შეიყვანე ასაკი:");
+let address = prompt("შეიყვანე მისამართი:");
+
+console.log(`მე ვარ ${firstName} ${lastName}, ვარ ${age} წლის და ვცხოვრობ ${address}-ში.`);
+
+// 2
+let number = 1;
+
+if (number > 0) {
+    console.log("positive");
+} else if (number < 0) {
+    console.log("negative");
+} else {
+    console.log("zero");
+}
