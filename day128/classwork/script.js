97a42ae0051e111e8c38e3890540dbed4eab79c8
giorgikandelaki123 <gio.kand2013@gmail.com>
2026-09-24 20:51:49 +0400

    1

---
+++ day128/classwork/script.js
@@ -0,0 +1,13 @@
+// 1)გადაიყვანეთ ტერნარში
+// if (favoritePhrase === 'Love That!') {
+//     console.log('I love that!');
+// } else {
+//     console.log("I don't love that!");
+// }
+
+console.log(favoritePhrase === 'Love That!' ? 'I love that!' : "I don't love that!");
+
+
+// 2)შექმენი ცვლადი სადაც შეინახავ რიცხვს,შეამოწმე ლუწია თ კენტი ტერნარით.
+let number = 67;
+console.log(number % 2 === 0 ? "ლუწია" : "კენტია");
